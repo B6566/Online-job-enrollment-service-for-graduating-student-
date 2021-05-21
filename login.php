7e8fe975d089bfde46d4d547f16b2888b7536b6b
B6566 <82933338+B6566@users.noreply.github.com>
2021-05-21 16:23:06 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -1,28 +1,50 @@
 <?php
-	session_start();
-	
-	$con = mysqli_connect('localhost', 'root', '');
+session_start(); 
+include "db_conn.php";
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+	 function validate($data){
+	 	$data = trim($data);
+	 	$data = stripslashes($data);
+	 	$data = htmlspecialchars($data); 
+	 	return $data; 
+	 }
+ 	echo $_POST['uname'];
+  	$uname = validate($_POST['uname']);
+  	$pass = validate($_POST['password']);
 
-	mysqli_select_db($con, 'barsi');
+ 	if (empty($uname)) { 
 
+ 	  header("Location: index.php?error=User Name is required");
+	  exit();    
+	}else if(empty($pass)){
+	  header("Location: index.php?error=Password is required"); 
+	   exit();
+ 	}else{
+  	    $sql ="SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
 
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+  	    $result = mysqli_query($conn, $sql);
+       
+       if (mysqli_num_rows($result) === 1){
+       	    $row = mysqli_fetch_assoc($result);
+       	    if ($row['user_name'] === $uname && $row['password'] === $pass){
+       	        $_SESSION['user_name'] =$row['user_name'];
+       	        $_SESSION['name'] =$row['name'];
+       	        $_SESSION['id'] =$row['id']; 
+       	        header("Location: home.php"); 
+	             exit(); 
 
-	$s = " select * from user where username = '$username' && password = '$password'";
-	 
-	$result = mysqli_query($con, $s);
+       	    }else{
+       	         header("Location: index.php?error=Incorrect User name or password"); 
+	             exit(); 
+	         }
 
-	$num = mysqli_num_rows($result);
+       }else{
+       	    header("Location: index.php?error=Incorrect User name or password"); 
+	        exit();}
+	 }
 
-	if ($num == 1){
-
-		$_SESSION['logged_in'] = true;
-		header("Location: ../student.php");
-		$_SESSION['un'] = $username;
-		}else{
-		header("location:../index.php");
-	}
-	
-
+}else{ 
+	header("Location: index.php");
+ 	exit();
+} 
 ?>
